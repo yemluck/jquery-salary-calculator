2fd919df33b6928c6e8c678f23d9bcc6c507523d
Chris Agbolade <yemluck@gmail.com>
2021-12-11 20:17:12 -0600

    bootstrap added, global variable created

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 $(document).ready( onReady )
 
+// Global employee list
+let employeeList = []
+
+
+
+
 function onReady(){
-    console.log('in onReady');
+    //console.log('in onReady');
 
     $('#salaryForm').on('submit', recordSalary)
 }
@@ -10,5 +16,26 @@ function recordSalary(event){
     // Prevent form from causing app to reload
     event.preventDefault();
     console.log('salary record added');
+
+    // grab data from form inputs
+
+    let fName = $('#fName').val();
+    let lName = $('#lName').val();
+    let id = $('#id').val();
+    let title = $('#title').val();
+    let annualSalary = Number($('#annualSalary').val());
+
+    let employee = {
+        firstName: fName,
+        lastName: lName,
+        id: id,
+        title: title,
+        annualSalary: annualSalary
+    };
+
+    // add each employee to the employeeList
+    employeeList.push(employee);
+
+    console.log('employee', employee)
     
 }
