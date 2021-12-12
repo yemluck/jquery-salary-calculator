7af58848129e207ee0b134a3082f442d8d5d0427
Chris Agbolade <yemluck@gmail.com>
2021-12-11 19:48:49 -0600

    DOM styled

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready( onReady )
 
 function onReady(){
-    console.log('in onReady')
+    console.log('in onReady');
+
+    $('#salaryForm').on('submit', recordSalary)
+}
+
+function recordSalary(event){
+    // Prevent form from causing app to reload
+    event.preventDefault();
+    console.log('salary record added');
+    
 }
